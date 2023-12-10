8533d5b2102ea794ac41ec8c789f4d02bcb71a5b
ProgrammingBotsInBinary <austinwhall.02@gmail.com>
2023-12-09 23:28:47 -0500

    Update Menu.js

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Menu({ setCategory, activeCategory }) {
   const categories = ['General', 'Business', 'Entertainment', 'Health', 'Science', 'Sports', 'Technology'];
+
+  useEffect(() => {
+    setCategory('General');
+    localStorage.setItem('activeCategory', 'General');
+  }, [setCategory]);
+
+  const handleButtonClick = (category) => {
+    setCategory(category);
+    localStorage.setItem('activeCategory', category);
+  };
 
   return (
     <div className="Menu">
       {categories.map((category, index) => (
         <button
           key={index}
-          onClick={() => setCategory(category)}
+          onClick={() => handleButtonClick(category)}
           className={category === activeCategory ? 'active' : ''}
         >
           {category}
